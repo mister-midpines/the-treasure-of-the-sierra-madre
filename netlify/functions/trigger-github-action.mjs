a94604aa4f5d3d07deb1fc33e176bd12ad96681d
mister-midpines <swooding@gmail.com>
2025-12-21 00:49:01 -0800

    Update trigger-github-action.mjs

--- netlify/functions/trigger-github-action.mjs
+++ netlify/functions/trigger-github-action.mjs
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 const GITHUB_PAT = process.env.GITHUB_TOKEN;
 const GITHUB_OWNER = 'mister-midpines';
-const GITHUB_REPO = 'jp';
+const GITHUB_REPO = 'the-treasure-of-the-sierra-madre';
 const WORKFLOW_FILENAME = 'advance_image.yml';
 const GIT_BRANCH = 'main';
 
